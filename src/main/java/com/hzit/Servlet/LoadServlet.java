7c504212f182b52a21aa4070076150949578ce2f
峰 <1364291059@qq.com>
2017-03-01 14:39:27 +0800

    RandomName

--- src/main/java/com/hzit/Servlet/LoadServlet.java
+++ src/main/java/com/hzit/Servlet/LoadServlet.java
@@ -52,58 +52,17 @@ public class LoadServlet extends HttpServlet {
                 i++;
             }
             //使用文件输出流输出文件
-            FileOutputStream fileOutputStream = new FileOutputStream("C:/"+teaName+".xls");
+            //FileOutputStream fileOutputStream = new FileOutputStream("C:/"+teaName+".xls");
             //response.setHeader("content-disposition", "attachment;filename=" + URLEncoder.encode(teaName, "UTF-8")+".xls");
-            workbook.write(fileOutputStream);
+           // workbook.write(fileOutputStream);
             //关闭文件输出流
-            fileOutputStream.close();
+            //fileOutputStream.close();
             response.setCharacterEncoding("UTF-8");
-            PrintWriter out=response.getWriter();
-            out.println("<html>\n" +
-                    "<head>\n" +
-                    "  <meta charset=\"utf-8\">\n" +
-                    "  <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">\n" +
-                    "  <title>导入成功</title>\n" +
-                    "  <meta name=\"description\" content=\"这是一个 index 页面\">\n" +
-                    "  <meta name=\"keywords\" content=\"index\">\n" +
-                    "  <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">\n" +
-                    "  <meta name=\"renderer\" content=\"webkit\">\n" +
-                    "  <meta http-equiv=\"Cache-Control\" content=\"no-siteapp\" />\n" +
-                    "  <link rel=\"icon\" type=\"image/png\" href=\"assets/i/favicon.png\">\n" +
-                    "  <link rel=\"apple-touch-icon-precomposed\" href=\"assets/i/app-icon72x72@2x.png\">\n" +
-                    "  <meta name=\"apple-mobile-web-app-title\" content=\"Amaze UI\" />\n" +
-                    "  <script src=\"assets/js/echarts.min.js\"></script>\n" +
-                    "  <link rel=\"stylesheet\" href=\"assets/css/amazeui.min.css\" />\n" +
-                    "  <link rel=\"stylesheet\" href=\"assets/css/amazeui.datatables.min.css\" />\n" +
-                    "  <link rel=\"stylesheet\" href=\"assets/css/app.css\">\n" +
-                    "  <script src=\"assets/js/jquery.min.js\"></script>\n" +
-                    "\n" +
-                    "</head>\n" +
-                    "\n" +
-                    "<body data-type=\"widgets\" style=\"background: #e9ecf3; \">\n" +
-                    "<script src=\"assets/js/theme.js\"></script>\n" +
-                    "<div class=\"row-content am-cf\" >\n" +
-                    "  <div class=\"widget am-cf\" style=\"background-color: #ffffff;border: 0px solid #ffffff\">\n" +
-                    "    <div class=\"widget-body\" >\n" +
-                    "      <div class=\"tpl-page-state\">\n" +
-                    "        <div class=\"tpl-page-state-title am-text-center tpl-error-title\">LoadExcelSuccessful</div>\n" +
-                    "        <div class=\"tpl-error-title-info\">Data has  been properly increased so you are seeing this as a fallback.</div>\n" +
-                    "        <div class=\"tpl-page-state-content tpl-error-content\">\n" +
-                    "\n" +
-                    "          <p>恭喜你,数据导入到excel成功！</p>\n" +
-                    "          <a class=\"am-btn am-btn-secondary am-radius tpl-error-btn\" href=\"javascript:history.go(-2)\">Back Home</a>\n" +
-                    "        </div>\n" +
-                    "      </div>\n" +
-                    "    </div>\n" +
-                    "  </div>\n" +
-                    "</div>\n" +
-                    "<script src=\"/assets/js/amazeui.min.js\"></script>\n" +
-                    "<script src=\"/assets/js/amazeui.datatables.min.js\"></script>\n" +
-                    "<script src=\"/assets/js/dataTables.responsive.min.js\"></script>\n" +
-                    "<script src=\"/assets/js/app.js\"></script>\n" +
-                    "\n" +
-                    "</body>\n" +
-                    "</html>");
+            //PrintWriter out=response.getWriter();
+            OutputStream outputStream=response.getOutputStream();
+            workbook.write(outputStream);
+            response.setHeader("content-disposition", "attachment;filename=" + URLEncoder.encode(teaName, "UTF-8")+".xls");
+
         }catch (Exception ex) {
             response.setCharacterEncoding("UTF-8");
             PrintWriter out=response.getWriter();
